25d7a2da7f4231da0feeb781de8fc767ad1aad66
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-23 16:46:24 +0530

    Added js code of password genetrator

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let inputSlider = document.getElementById("inputSlider");
 let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let number = document.getElementById("number");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
 
 //show input slider value
 sliderValue.textContent = inputSlider.value;
@@ -7,3 +13,38 @@ sliderValue.textContent = inputSlider.value;
 inputSlider.addEventListener("input", () => {
   sliderValue.textContent = inputSlider.value;
 });
+
+genBtn.addEventListener("click", () => {
+  passBox.value = generatePassword();
+});
+
+let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerChar = "abcdefghijklmnopqrstuvwxyz";
+let allNumbers = "0123456789";
+let allSymbols = "~!@#$%^&*";
+// Function to genetrate  password
+
+function generatePassword() {
+  let genPassword = "";
+  let allChars = "";
+
+  allChars += lowercase.checked ? lowerChar : "";
+  allChars += uppercase.checked ? upperChar : "";
+
+  allChars += number.checked ? allNumbers : "";
+
+  allChars += symbols.checked ? allSymbols : "";
+
+  if (allChars == " " || allChars.length == 0) {
+    return genPassword;
+  }
+
+  let i = 1;
+  while (i <= inputSlider.value) {
+    genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+
+    i++;
+  }
+
+  return genPassword;
+}
