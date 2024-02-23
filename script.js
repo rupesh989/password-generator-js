8620ca8b0ffe40d15c1ab1236c159a147578147b
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-23 16:13:43 +0530

    Added js code for sliders

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
 
+//show input slider value
+sliderValue.textContent = inputSlider.value;
+
+inputSlider.addEventListener("input", () => {
+  sliderValue.textContent = inputSlider.value;
+});
